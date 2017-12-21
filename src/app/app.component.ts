f4d01991815a25ca035aeff08e7dde2858126ee0
Jon Graca <jon.graca@gmail.com>
2017-12-21 11:34:28 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  favoriteThing: string;
+  thingClicked: string;
+  thingAdded: string;
+  selectedThing: string;
+
+  favoriteThingsArray = ['Backpacking', 'Beers', 'Brats', 'Your Mom'];
+
+  clickedThing(thingClicked): void {
+    console.log('Clicked Thing: ' + thingClicked);
+    this.selectedThing = thingClicked;
+  }
+
+  addFavoriteThing(thingAdded) {
+    console.log('Thing Added: ' + thingAdded);
+    this.favoriteThingsArray.push(thingAdded);
+    return false;
+  }
+
 }
