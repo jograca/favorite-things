7520c24b7eaf24d4f9e554d342102fbadcebcc2b
jon.graca@gmail.com <jon.graca@gmail.com>
2017-12-21 13:29:29 -0500

    Updates with new Component

--- src/app/components/editfavoritething/editfavoritething.component.ts
+++ src/app/components/editfavoritething/editfavoritething.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-editfavoritething',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editfavoritething.component.css']
 })
 export class EditfavoritethingComponent implements OnInit {
+
+  selectedThing: object;
 
   constructor() { }
 
