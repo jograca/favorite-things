516b60f1024da71d051452098cdadf5dce9d818e
jon.graca@gmail.com <jon.graca@gmail.com>
2017-12-21 13:46:04 -0500

    Updates to build scripts

--- src/app/components/editfavoritething/editfavoritething.component.ts
+++ src/app/components/editfavoritething/editfavoritething.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class EditfavoritethingComponent implements OnInit {
 
+  @Input()
   selectedThing: object;
 
   constructor() { }
